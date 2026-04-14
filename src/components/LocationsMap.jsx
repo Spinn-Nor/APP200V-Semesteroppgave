b490773e1e0d5cd1450976ff3a4a8af53124da74
Bendik Wangen <228041@usn.no>
2026-04-14 11:15:16 +0200

    enabled scroll zoom for embedded map

--- src/components/LocationsMap.jsx
+++ src/components/LocationsMap.jsx
@@ -20,7 +20,7 @@ function Locations() {
                         height: '95%',
                         border: '0',
                     }}
-                    allowFullScreen allow="geolocation" src="//umap.openstreetmap.fr/en/map/app200v-blueberry-hotels_1390929?scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true"></iframe><p><a href="//umap.openstreetmap.fr/en/map/app200v-blueberry-hotels_1390929?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true">
+                    allowFullScreen allow="geolocation" src="//umap.openstreetmap.fr/en/map/app200v-blueberry-hotels_1390929?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true"></iframe><p><a href="//umap.openstreetmap.fr/en/map/app200v-blueberry-hotels_1390929?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&editMode=disabled&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=none&captionBar=false&captionMenus=true">
                         {/* See full screen */}
                     </a></p>
             </div>
